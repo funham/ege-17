587a7d2573fba0a3bb3148d92e1d46f343326eea
D.Chentsov <lol@mail.net>
2021-11-09 13:43:08 +0300

    outputs only count of pairs

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@ using namespace std;
     + 1. считатьи запомнить входные числа
     + 2. вывести все пары
     + 3. вместо вывода пары посчитать ее произведение
-    4. вывести только пары, произведение которых кратно 21
-    5. только кол-во пар
+    + 4. вывести только пары, произведение которых кратно 21
+    + 5. только кол-во пар
 */
 
 void count_of_prod()
@@ -24,19 +24,19 @@ void count_of_prod()
         cin >> nums[i];
     }
 
+    int corr_count = 0;
+
     for (int i = 0; i < n - 1; i++)
     {
         for (int j = i + 1; j < n; j++)
         {
             int prod = nums[i] * nums[j];
             
-            if (prod % 21 == 0)
-            {
-                cout << prod << endl;
-            }
-            
+            corr_count += (prod % 21 == 0);
         }
     }
+
+    cout << corr_count;
 }
 int main()
 {
