6706b4da3cd6edf9670e73bdba0153b5dfa8b21b
D.Chentsov <lol@mail.net>
2021-11-09 14:11:55 +0300

    effective solution ready

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <inttypes.h>
 using namespace std;
 
 void count_of_prod()
@@ -9,7 +10,26 @@ void count_of_prod()
     3. Инкрементировать соответствующий счетчик
     4. Вывести сумму всех счетчиков
     */
-    cout << "Hello, world!" << endl;
+
+   uint32_t n; cin >> n;
+   uint32_t nn, n7, n3, n21;
+   
+   nn = n7 = n3 = n21 = 0;
+
+   for (uint32_t i = 0; i < n; i++)
+   {
+        int a; cin >> a;
+        if (a % 3 == 0)
+            n3++;
+        else if (a % 7 == 0)
+            n7++;
+        else if (a % 21 == 0)
+            n21++;
+        else
+            nn++;
+   }
+
+   cout << n21*(n21 - 1)/2 + n21*n3 + n21*n7 + n21*nn + n7 * n3;
 }
 
 int main()
