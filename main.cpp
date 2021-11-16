bcdef5332e9d858a3131f1adaefa4333b788ea6b
D.Chentsov <lol@mail.net>
2021-11-16 12:36:25 +0300

    reads numbers

--- main.cpp
+++ main.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
+
+/*
+
+1. + считатьи запомнить входные числа
+2. вывести все пары
+3. вместо вывода пары посчитать ее сумму
+4. вывести только нечетные суммы
+5. вывести только максималную сумму
+
+*/
+
+void max_sum()
+{
+    int n = 0;
+    cin >> n;
+
+    vector<int> nums(n, 0);
+    
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+}
 
 int main()
 {
