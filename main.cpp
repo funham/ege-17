04d6d8e3266c867b87c4a0852f7a9c32698b8e82
D.Chentsov <lol@mail.net>
2021-11-16 14:13:22 +0300

    effective solvation ready

--- main.cpp
+++ main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
 using namespace std;
 
+void max_sum_eff()
+{
+    int max_odd = -9999, max_even = -9999;
+
+    int a; cin >> a;
+    
+    while(a != -9999)
+    {
+        if (a % 2)
+        {
+            max_odd = std::max(max_odd, a);
+        }
+        else
+        {
+            max_even = std::max(max_even, a);
+        }
+        cin >> a;
+    }
+
+    if (max_odd != -9999 && max_even != -9999)
+    {
+        cout << max_odd + max_even << endl;
+    }
+    else
+    {
+        cout << -1 << endl;
+    }
+}
+
+
 int main()
 {
-    cout << "Hello, world!" << endl;
+    max_sum_eff();
 }
