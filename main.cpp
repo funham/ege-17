5a2ceb091f02ac205ba7ceb1fd448ffa197d6d64
D.Chentsov <lol@mail.net>
2021-11-30 12:43:56 +0300

    outputs min pair

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 using namespace std;
 
+void do_sum()
+{
+    int N; cin >> N;
+    int min_sum = INT16_MAX;
+    int min1, min2;
+    min1 = min2 = INT16_MAX;
+
+    for (int i = 0; i < N; i++)
+    {
+        int a; cin >> a;
+        min2 = std::min(min2, a);
+        min1 = std::min(min1, min2);
+    }
+
+    min_sum = min1 + min2;
+
+    cout << min_sum;
+}
+
 int main()
 {
-    cout << "Hello, world!" << endl;
+    do_sum();
 }
