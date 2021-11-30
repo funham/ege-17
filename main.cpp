9c44e2655957e6d93e30b2cc77a88c71dcf7facc
D.Chentsov <lol@mail.net>
2021-11-30 12:27:35 +0300

    inputs and outputs

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
 
+void do_sum()
+{
+    int N; cin >> N;
+
+    int *a = new int[N];
+
+    for (auto i = 0; i < N; i++)
+    {
+        cin >> a[i];
+    }
+
+    for (auto i = 0; i < N; i++)
+    {
+        cout << a[i] << endl;
+    }
+    delete [] a;
+}
+
 int main()
 {
     cout << "Hello, world!" << endl;
