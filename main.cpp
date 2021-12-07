47ba6224348c3d5c370abc87090164a7e9281f61
xs1nus <fun1ham@gmail.com>
2021-12-07 11:34:34 +0300

    debuged on izf

--- main.cpp
+++ main.cpp
@@ -3,27 +3,27 @@
 
 void count_of_prod()
 {
-    int n;
+    uint64_t n;
     std::cin >> n;
-    int nn, n2, n5, n7, n10, n14, n35, n70;
+    uint64_t nn, n2, n5, n7, n10, n14, n35, n70;
 
     nn = n2 = n5 = n7 = n10 = n14 = n35 = n70 = 0;
 
-    for (int i = 0; i < n; i++)
+    for (uint64_t i = 0; i < n; i++)
     {
         int a;
-        cin >> a;
-        if (a % 70 == 0)
+        std::cin >> a;
+        if (a % 30 == 0)
             n70++;
-        else if (a % 35 == 0)
+        else if (a % 15 == 0)
             n35++;
-        else if (a % 14 == 0)
-            n14++;
         else if (a % 10 == 0)
+            n14++;
+        else if (a % 6 == 0)
             n10++;
-        else if (a % 7 == 0)
-            n7++;
         else if (a % 5 == 0)
+            n7++;
+        else if (a % 3 == 0)
             n5++;
         else if (a % 2 == 0)
             n2++;
