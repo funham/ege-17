52f4bbffd7c147df1653be466428f5ffb123b5e4
xs1nus <fun1ham@gmail.com>
2021-12-14 14:01:01 +0300

    calculates maximums in each set of numbers (rows in array)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
 #include <inttypes.h>
+#include <stddef.h>
 
 void func()
 {
+    int maximums[6][2]{};
+
+    int N;
+    std::cin >> N;
+
+    int a;
+    std::cin >> a;
+
+    for (int i = 1; i < N; i++)
+    {
+        auto &minmax = maximums[i % 6][0];
+        auto &maxmax = *(&minmax + 1); // just next element
+
+        // kinda sorting a and maximums
+        if (a > minmax)
+        {
+            minmax = a;
+        }
+        if (minmax > maxmax)
+        {
+            std::swap(minmax, maxmax);
+        }
+    }
 }
 
 int main()
