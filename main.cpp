df27bafab4b81f1d2e989c6a0ef67d384477eb14
xs1nus <fun1ham@gmail.com>
2021-12-14 13:10:16 +0300

    allocates memory

--- main.cpp
+++ main.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 #include <inttypes.h>
 
-void func()
+void num_sum_max_distance_ineff()
 {
+    int N;
+    std::cin >> N;
+
+    int *a = new int[N]();
+
+    for (int i = 0; i < N; i++)
+    {
+        std::cout << a[i] << std::endl;
+    }
+
+    delete[] a;
 }
 
 int main()
 {
-    func();
+    num_sum_max_distance_ineff();
     return 0;
 }
