2035b561ee1265fe41cc463bf5d87f8949b4f6a9
xs1nus <fun1ham@gmail.com>
2021-12-18 20:27:21 +0300

    added basics

--- main.cpp
+++ main.cpp
@@ -1,12 +1,36 @@
 #include <iostream>
-#include <inttypes.h>
 
-void func()
+void do_sum()
 {
+    int N;
+    std::cin >> N;
+    int min_sum = INT16_MAX;
+    int min_v = INT16_MAX;
+    int buff[4] = {};
+
+    for (int i = 0; i < 4; i++)
+    {
+        std::cin >> buff[i];
+    }
+
+    for (int i = 4; i < N; i++)
+    {
+        int a;
+        std::cin >> a;
+        int min_cand = buff[0];
+        for (int i = 0; i < 3; i++)
+        {
+            buff[i] = buff[i + 1];
+        }
+        buff[3] = a;
+        min_v = std::min(min_cand, min_v);
+        min_sum = std::min(min_sum, min_v + buff[3]);
+    }
+
+    std::cout << min_sum;
 }
 
 int main()
 {
-    func();
-    return 0;
+    do_sum();
 }
