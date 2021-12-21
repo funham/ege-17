249ffdc81a71d86746972d5240558b46b7d72741
funham <fun1ham@gmail.com>
2021-12-21 12:38:45 +0300

    sorts complimentary minimums

--- main.cpp
+++ main.cpp
@@ -1,12 +1,38 @@
 #include <iostream>
 #include <inttypes.h>
+#include <memory.h>
 
-void func()
+const int MULT = 4;
+
+void numbers_min_sum3()
 {
+    int n; std::cin >> n;
+
+    int cntrs[MULT] = {};
+
+    // 3 - count of minimums
+    int mins[MULT][3];
+    memset(mins, INT_MAX, sizeof(int) * 3 * 4);
+
+
+    for (int i = 0; i < n; i++)
+    {
+        int a; std::cin >> a;
+
+        cntrs[a % MULT] += 1;
+        
+        for (int &ord_min : mins[a % MULT])
+        {
+            if (a < ord_min)
+            {
+                std::swap(a, ord_min);
+            }
+        }
+    }
 }
 
 int main()
 {
-    func();
+    numbers_min_sum3();
     return 0;
 }
