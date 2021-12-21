52b8c870b7a3a644ca3c8cfb691e0d54c0e4d905
funham <fun1ham@gmail.com>
2021-12-21 13:42:48 +0300

    added algorithm

--- main.cpp
+++ main.cpp
@@ -6,6 +6,17 @@
 
 const int MULT = 4;
 
+int min_sum = 4000;
+
+
+void refresh_min(int b)
+{
+    if (b < min_sum)
+    {
+        min_sum = b;
+    }
+}
+
 void numbers_min_sum3()
 {
     int n; std::cin >> n;
@@ -18,7 +29,7 @@ void numbers_min_sum3()
     {
         for (int j = 0; j < 3; j++)
         {
-            mins[i][j] = 1002;
+            mins[i][j] = 4000;
         }
     }
 
@@ -47,18 +58,41 @@ void numbers_min_sum3()
     }
 
     /* 0 = [...] % 4
-    0 + 0 + 0
-    0 + 1 + 3
-    0 + 2 + 2
-    1 + 1 + 2
-    1 + 3 + 0
+    0 + 0 + 0 done
+    0 + 1 + 3 done
+    0 + 2 + 2 done
+    1 + 1 + 2 done
     3 + 3 + 2
     */
-   
-    for (int i = 0; i < MULT; i++)
+
+    int &m0 = mins[0][0];
+    int &m1 = mins[1][0];
+    int &m2 = mins[2][0];
+    int &m3 = mins[3][0];
+
+    int &m0s = mins[0][1];
+    int &m1s = mins[1][1];
+    int &m2s = mins[2][1];
+    int &m3s = mins[3][1];
+
+    int curr_sum = 0;
+
+    for (int i = 0; i < 3; i++)
     {
-        mins[i];
+        curr_sum = mins[0][i];
     }
+
+    refresh_min(curr_sum);
+
+    refresh_min(m0 + m1 + m3);
+
+    refresh_min(m0 + m2 + m2s);
+    
+    refresh_min(m1 + m1s + m2);
+
+    refresh_min(m3 + m3s + m2);
+
+    std::cout << min_sum;
 }
 
 int main()
